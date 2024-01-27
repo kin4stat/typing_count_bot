54980dddedafda28c3ae78b8582e5ce1ef2bf0d3
kin4stat <dkustov4@gmail.com>
2024-01-27 12:15:33 +0300

    Disable online updater

--- main.py
+++ main.py
@@ -194,7 +194,7 @@ async def update_online():
 
 async def main():
     await client.start()
-    asyncio.create_task(update_online())
+    #asyncio.create_task(update_online())
 
     config = uvicorn.Config("main:app", host="0.0.0.0", port=25424, log_level="info")
     server = uvicorn.Server(config)
